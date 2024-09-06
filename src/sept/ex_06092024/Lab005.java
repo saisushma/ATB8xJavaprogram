cbb6d2f2749ce41b8736ebb0af7c2b9b0fb73734
saisushma <chilukurisaisushma@gmail.com>
2024-09-06 08:11:13 +0530

    pushing the new files

---
+++ src/sept/ex_06092024/Lab005.java
@@ -0,0 +1,10 @@
+package sept.ex_06092024;
+
+public class Lab005 {
+
+    public static void main(String[] sai1) {
+        System.out.println("giving different args name to check !");
+
+
+    }
+}
---
+++ src/sept/ex_06092024/Lab008.java
@@ -0,0 +1,18 @@
+//below code is package : folder in sept
+package sept.ex_06092024;
+
+/**
+ *
+ * checking the comments
+ *
+ * */
+
+public class Lab008 {
+    //this is the main function, JVM starts from here
+    public static void main(String[] args) {
+        System.out.println("verifying the comments section");
+    // trying to print with comments
+
+
+    }
+}

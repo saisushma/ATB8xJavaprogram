d117b04d4343c82b6cfb690923c6232017462200
saisushma <chilukurisaisushma@gmail.com>
2024-09-09 08:07:00 +0530

    commiting the datatype

---
+++ src/sept/ex_0909204/Lab009.java
@@ -0,0 +1,22 @@
+package sept.ex_0909204;
+
+public class Lab009 {
+
+    public static void main(String[] args){
+        //single line comment
+
+
+        /*
+        mutiple line comments
+        keywords & identifiers
+        those are predefined meaning in java programs
+         */
+
+        /**
+         * for documentation purpose
+         *
+         **/
+
+
+    }
+}
---
+++ src/sept/ex_0909204/Lab010.java
@@ -0,0 +1,9 @@
+package sept.ex_0909204;
+
+public class Lab010 {
+    public static void main(String[] args) {
+        //variables and datatypes, identifiers
+        boolean is_sai_available_today = true;
+
+    }
+}

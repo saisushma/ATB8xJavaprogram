ef8afeb88d79c8f766dcf03e66fdfbfa9a47fd69
saisushma <chilukurisaisushma@gmail.com>
2024-09-15 12:39:04 +0530

    commiting the assignment

---
+++ src/sept/ex_13092024_UserdefinedDatatypes/Task2_calculator.java
@@ -0,0 +1,20 @@
+package sept.ex_13092024_UserdefinedDatatypes;
+
+public class Task2_calculator {
+
+    public static void main(String[] args) {
+
+
+        float a = 50;
+        float b = 10;
+
+        System.out.printf("%f + %f = %f \n", a,b,a+b);
+        System.out.printf("%f - %f = %f \n ", a,b,a-b);
+        System.out.printf("%f * %f = %f \n", a,b,a*b);
+        System.out.printf("%f / %f = %f \n", a,b,a/b);
+        System.out.printf("%f %% %f = %f \n", a,b,a/b);
+
+
+
+    }
+}

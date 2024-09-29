bd0667fa09dcfd52d97f5183215d3cf4d9fee297
saisushma <chilukurisaisushma@gmail.com>
2024-09-29 22:57:03 +0530

    commiting the task - 27 sept

--- src/sept/Tasks/sept27092024/Task3.java
+++ src/sept/Tasks/sept27092024/Task3.java
@@ -4,13 +4,13 @@ import java.util.Scanner;
 
 public class Task3 {
     public static void main(String[] args) {
+//Write a Java program that calculates the sum of numbers from 1 to 100 using a while loop.
 
         Scanner sc = new Scanner(System.in);
         System.out.println("enter the num ");
 
         int num = sc.nextInt();
         int sum=0;
-     //   int i=1;
 
         while(num>0)
         {
